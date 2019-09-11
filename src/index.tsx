6e3d291618621f00d2e63fa3415e956ba45aa64b
imzengyang <zzy15505919313@gmail.com>
2019-09-11 20:35:19 +0800

    Initial commit initial project

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Appcomponent from './components/app'
+
+ReactDOM.render(
+    <Appcomponent />,
+    document.getElementById('app')
+);
---
+++ src/main.ts
@@ -0,0 +1,43 @@
+
+import {app, BrowserWindow} from 'electron'
+
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.loadFile(__dirname + '/index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
