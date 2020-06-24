0b28a04f2c1a0184c9c3b08e8362e59b7fdbd1bd
zack <390021310@qq.com>
2020-06-24 19:04:28 +0800

    fix 多次加载app列表，提升性能

--- src/components/android/apps.tsx
+++ src/components/android/apps.tsx
@@ -4,23 +4,18 @@ import { ADB } from '../../android/tools/androiddevice'
 const { Option } = Select;
 
 export default function ListAppsComponents() {
-    const [apps, setApps] = React.useState([]);
-    function handleFocus() {
-        // console.log(ADB.listDevices())
-        const adb = new ADB(ADB.listDevices()[0]);
-        // console.log(adb.listInstalledApp())
-        const allApps = adb.listInstalledApp();
-        setApps(allApps);
-    }
+    const adb = new ADB(ADB.listDevices()[0]);
+    // console.log(adb.listInstalledApp())
+    const allApps = adb.listInstalledApp();
+    
     return (
         <>
             <Select defaultValue="no devices" style={{ width: '100%', marginTop: 3, marginBottom: 6 }}>
                 <Option value="no devices">请选择设备</Option>
-
             </Select>
-            <Select defaultValue="no devices" showSearch style={{ width: '100%' }} onFocus={handleFocus}>
+            <Select defaultValue="no devices" showSearch allowClear style={{ width: '100%' }}>
                 <Option value="no devices">请选择被测App</Option>
-                {apps.map((app, index) => (<Option key={index} value={app}>{app}</Option>))}
+                {allApps.map((app, index) => (<Option key={index} value={app}>{app}</Option>))}
             </Select>
             <Button type="primary" block style={{ marginTop: 4, marginBottom: 4 }}>开始测试</Button>
         </>
